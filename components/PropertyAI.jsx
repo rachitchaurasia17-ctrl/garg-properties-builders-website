7954b4783a3888b99e3bc58bf342cdf209f16962
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 16:00:28 +0530

    Fix Realty suffix and update correct address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Garg Prope
 
 Firm facts:
 - Firm: Garg Properties & Builders
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: S.C.O. No. 1040, Springdale Market, Sec. 123, Greater Mohali, Sunny Enclave
 - Phone: +91 XXXXX XXXXX
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
